a6d31e93b9b1bb12194059eb736d12f341555bb8
Your Name <you@example.com>
2020-09-25 08:44:21 -0400

    grid set up, generations count feature added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,140 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback, useRef, useState } from 'react';
 import './App.css';
+import produce, { produceWithPatches } from 'immer';
+
+let numRows = 25;
+let numCols = 25;
+let count = 0
+
+let operations = [
+  [0, 1],
+  [0, -1],
+  [1,-1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0]
+];
+
+let generateEmptyGrid = () => {
+  let rows = [];
+  count = 0
+
+  for (let i = 0; i < numRows; i++){
+    rows.push(Array.from(Array(numCols), () => 0))
+  }
+  return rows
+}
 
 function App() {
+  let [grid, setGrid] = useState(() => {
+    return generateEmptyGrid()
+  });
+
+  let [running, setRunning] = useState(false);
+
+
+  let runningRef = useRef(running);
+  runningRef.current = running
+
+  // keeps the function unchaged, so it doesn't re-render every time
+  let runSimulation = useCallback(() => {
+    if (!runningRef.current) {
+      return
+    }
+
+    setGrid(g => {
+      return produce(g, gridCopy => {
+        for (let i = 0; i < numRows; i++) {
+          for (let j = 0; j < numCols; j++){
+            let neighbors = 0;
+            operations.forEach(([x, y]) => {
+              let newI = i + x;
+              let newJ = j + y;
+              if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) {
+                neighbors += g[newI][newJ]
+              }
+            })
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][j] = 0
+            } else if (g[i][j] === 0 && neighbors === 3) {
+              gridCopy[i][j] = 1;
+            }
+
+          }
+        }
+      });
+    });
+    count += 1
+    setTimeout(runSimulation, 1000)
+  }, [])
+
+  console.log(grid)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Game of Life</h1>
+    <section className="main">
+      <div 
+        style={{
+        display: 'grid',
+        gridTemplateColumns: `repeat(${numCols}, 30px)`
+      }}>
+        {grid.map((rows, i) => 
+          rows.map((col, j) => 
+            <div
+            onClick={() => {
+              let newGrid = produce(grid, gridCopy => {
+                gridCopy[i][j] = grid[i][j] ? 0 : 1;
+              })
+              setGrid(newGrid)
+            }}
+            key={`${i} - ${j}`}
+            style={{
+            width: 30, 
+            height: 25, 
+            backgroundColor: grid[i][j] ? 'blueviolet' : undefined,
+            border: 'solid 1px black'
+            }}/>
+        ))}
+      </div>
+      <div className="game">
+        <div className="rules">
+          Rules Placeholder
+        </div>
+        
+        <div className="controls">
+          <button
+          onClick={() => {
+            setRunning(!running)
+            if (!running) {
+              runningRef.current = true
+              runSimulation()}}
+            }
+          style={{width: 100, margin: 10}}>{running ? 'Stop' : "Start"}</button>
+          <button
+          onClick={() => {
+            setGrid(generateEmptyGrid());
+          }}
+          style={{width: 100, margin: 10}}>Clear</button>
+          <button style={{width: 100, margin: 10}}>Pause</button>
+          <button 
+          onClick={() => {
+            let rows = [];
+            count = 0
+
+            for (let i = 0; i < numRows; i++){
+              rows.push(Array.from(Array(numCols), () => Math.random() > .5 ? 1 : 0))
+            }
+            return setGrid(rows)
+          }}
+          style={{width: 100, margin: 10}}>Random</button>
+          <h3>Generations: {count}</h3>
+        </div>
+      </div>
+    </section>
+    </>
   );
 }
 
